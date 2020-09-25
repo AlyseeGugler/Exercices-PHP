025f6b50b5ac5b32db2fc824ca641de0e8f76976
AlyseeGugler <66822519+AlyseeGugler@users.noreply.github.com>
2020-09-25 11:35:12 +0200

    Update Exercice1.php

--- Exercice1.php
+++ Exercice1.php
@@ -1,20 +1,13 @@
-<html>
-    <head>
-        <title>Exercice 1</title>
-    </head>
-    <body>
-        <?php
-        function somme($n){
-            $somme=0;
-            for ($i=1;$i<=$n;$i++){
-                $somme=$somme+$i;
-            }
-            echo $somme;
-            echo ' ';
-        }
-        somme(5);
-        somme(2);
-        somme(8);
-        ?>
-    </body>
-</html>
+<?php
+function somme(int $n){
+    $somme=0;
+    for ($i=1;$i<=$n;$i++){
+        $somme=$somme+$i;
+    }
+    echo $somme;
+    echo ' ';
+}
+somme(5);
+somme(2);
+somme(8);
+?>
